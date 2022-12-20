162dbe3524009074092bb91e314178b9d090c14a
Dardaul Hoque <bsse1126@iit.du.ac.bd>
2022-12-20 14:33:07 +0600

    extension method overload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Testing;
 using Logging;
+using learn_dotnet.models;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
         "Hola Amigos Buenos Dias".Dump();
+        "Say My Name".Dump("--> Heisenberg");
     }
 
     public static void NewHandler(object? source, EventArgs eventArgs)
--- models/Dump.cs
+++ models/Dump.cs
@@ -1,7 +1,14 @@
+namespace learn_dotnet.models;
+
 public static class DumpString
 {
     public static void Dump(this string s)
     {
-        System.Console.WriteLine(s);
+        Console.WriteLine(s);
+    }
+    public static void Dump(this string s, string id)
+    {
+        Console.WriteLine(s);
+        Console.WriteLine(id);
     }
 }
